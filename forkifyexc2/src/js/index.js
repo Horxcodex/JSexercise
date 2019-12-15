37e5c46a99fc855ef220665182abc193dbe2a6cf
Horxcodex <fearofnear@gmail.com>
2019-12-15 12:31:28 +0700

    additional changes to clg

--- forkifyexc2/src/js/index.js
+++ forkifyexc2/src/js/index.js
@@ -6,34 +6,35 @@ const state = {};
 
 const controlSearch = async () => {
 	// 1). Get query from the view (yg kita ketik dari search bar)
-	console.log('STEP 1');
+	console.log('STEP 1 = Get query from the search bar');
 	const query = searchView.getInput();
-	//console.log(query);
+	console.log(query);
 	console.log(`getInput()`);
+	console.log(`STATE OBJECT :`) + console.log(state);
 
 	if (query) {
 		// 2). Create new Object ( Search object that we create in 1E is placed here on this step) and add it to state object. Tips: state.search = the object and add 'query' to the argument.
-		console.log('STEP 2');
+		console.log('STEP 2 = Create new Object');
 		state.search = new Search(query);
 		console.log(`new Search(query)`);
 
 		// 3). Prepare UI for result (add loading spiner, clear prev search results, we will do this on next lecture). Skip on this Alg
-		console.log('STEP 3');
+		console.log('STEP 3 = Prepare UI for result (add loading spiner, clear prev search results)');
 		searchView.clearInput();
 		console.log(`clearInput()`);
 		searchView.clearResults();
 		console.log(`clearResults()`);
 
 		// 4). Do the search, search for recipe.
-		console.log('STEP 4');
+		console.log('STEP 4 = Do the search, search for recipe');
 		await state.search.getResults();
 		console.log(`state.search.getResults()`);
 
 		//5). Show results to the UI, render results.
-		console.log('STEP 5');
+		console.log('STEP 5 = Show results to the UI, render results');
 		searchView.renderResults(state.search.result);
 		console.log(state.search.result);
-		console.log(`renderResults()`);
+		console.log(`renderResults() & limitRecipeTitle()`);
 	}
 };
 
