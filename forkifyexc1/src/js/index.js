ddc7705dfb29e13d8efb87b63324864067b11f0b
Horxcodex <fearofnear@gmail.com>
2019-12-30 11:58:43 +0700

    Building the Recipe Controller finish

--- forkifyexc1/src/js/index.js
+++ forkifyexc1/src/js/index.js
@@ -1,4 +1,5 @@
 import Search from './models/Search';
+import Recipe from './models/Recipe';
 import { elements, renderLoader, clearLoader } from './views/base';
 import * as searchView from './views/searchView';
 
@@ -18,13 +19,17 @@ const controlSearch = async (e) => {
 		searchView.clearResults();
 		renderLoader(elements.searchRes);
 
-		// 4). Do the search, search for recipe.
-		await state.Search.getResults();
+		try {
+			// 4). Do the search, search for recipe.
+			await state.Search.getResults();
 
-		// 5). Show results to the UI, render results.
-		//console.log(state.Search.result);
-		clearLoader();
-		searchView.renderResults(state.Search.result);
+			// 5). Show results to the UI, render results.
+			//console.log(state.Search.result);
+			clearLoader();
+			searchView.renderResults(state.Search.result);
+		} catch (error) {
+			alert('error loading Search!!');
+		}
 	}
 };
 
@@ -33,7 +38,7 @@ const controlPagination = (e) => {
 
 	if (ele) {
 		let goToPage = parseInt(ele.dataset.goto, 10);
-		console.log(goToPage);
+		//console.log(goToPage);
 		searchView.clearResults();
 		searchView.renderResults(state.Search.result, goToPage);
 	}
@@ -42,3 +47,36 @@ const controlPagination = (e) => {
 elements.searchForm.addEventListener('submit', controlSearch);
 
 elements.searchResPages.addEventListener('click', controlPagination);
+
+//---------------------------RECIPE CONTROLLER-------------------------------------------//
+
+const controlRecipe = async () => {
+	// 1. Get id from the URL
+	let id = window.location.hash.replace('#', '');
+	console.log(id);
+
+	if (id) {
+		// 2. Prepare UI for changes
+
+		// 3. Create new recipe object
+		state.Recipe = new Recipe(id);
+
+		try {
+			// 4. Get recipe data & parse the ingredients
+			await state.Recipe.getRecipe();
+			//console.log(state.Recipe.ingredients);
+
+			// 5. Calculte servings and time by calling the
+			state.Recipe.calcTime();
+			state.Recipe.calcServings();
+
+			// 6. Render recipe to UI.
+			console.log(state.Recipe);
+		} catch (error) {
+			alert('error loading recipe!!');
+		}
+	}
+};
+
+window.addEventListener('hashchange', controlRecipe);
+window.addEventListener('load', controlRecipe);
