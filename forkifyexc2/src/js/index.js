553ea2ea81ffc3bc71281829cc43aaba1166bee8
Horxcodex <fearofnear@gmail.com>
2020-01-05 14:58:00 +0700

    completed recipe constroller parse ingredients

--- forkifyexc2/src/js/index.js
+++ forkifyexc2/src/js/index.js
@@ -7,40 +7,47 @@ const state = {};
 
 const controlSearch = async () => {
 	// 1). Get query from the view (yg kita ketik dari search bar)
-	console.log('----STEP 1 = Get query from the search bar----');
+	//console.log('----STEP 1 = Get query from the search bar----');
 	const query = searchView.getInput();
-	console.log(query);
-	console.log(`getInput()`);
-	console.log(`STATE OBJECT :`) + console.log(state);
+	// testing
+	//const query = 'pizza';
+
+	// console.log(query);
+	// console.log(`getInput()`);
+	// console.log(`STATE OBJECT :`) + console.log(state);
 
 	if (query) {
 		// 2). Create new Object ( Search object that we create in 1E is placed here on this step) and add it to state object. Tips: state.search = the object and add 'query' to the argument.
-		console.log('----STEP 2 = Create new Object----');
+		//console.log('----STEP 2 = Create new Object----');
 		state.search = new Search(query);
-		console.log(`new Search(query)`);
+		//console.log(`new Search(query)`);
 
 		// 3). Prepare UI for result (add loading spiner, clear prev search results, we will do this on next lecture). Skip on this Alg
-		console.log('----STEP 3 = Prepare UI for result (add loading spiner, clear prev search results)----');
+		//console.log('----STEP 3 = Prepare UI for result (add loading spiner, clear prev search results)----');
 		searchView.clearInput();
-		console.log(`clearInput()`);
+		//console.log(`clearInput()`);
 		searchView.clearResults();
-		console.log(`clearResults()`);
+		//console.log(`clearResults()`);
 		renderLoader(elements.searchRes);
-		console.log(`renderLoader()`);
+		//console.log(`renderLoader()`);
 
-		// 4). Do the search, search for recipe.
-		console.log('----STEP 4 = Do the search, search for recipe----');
-		await state.search.getResults();
-		console.log(`state.search.getResults()`);
-		clearLoader();
-		console.log(`clearLoader()`);
+		try {
+			// 4). Do the search, search for recipe.
+			//console.log('----STEP 4 = Do the search, search for recipe----');
+			await state.search.getResults();
+			//console.log(`state.search.getResults()`);
+			clearLoader();
+			//console.log(`clearLoader()`);
 
-		//5). Show results to the UI, render results.
-		console.log('----STEP 5 = Show results to the UI, render results----');
-		searchView.renderResults(state.search.result);
-		console.log(state.search.result);
-		console.log(`renderResults(), renderRecipes() & limitRecipeTitle()`);
-		console.log(`createButtons() & renderButtons()`);
+			//5). Show results to the UI, render results.
+			//console.log('----STEP 5 = Show results to the UI, render results----');
+			searchView.renderResults(state.search.result);
+			//console.log(state.search.result);
+			//console.log(`renderResults(), renderRecipes() & limitRecipeTitle()`);
+			//console.log(`createButtons() & renderButtons()`);
+		} catch (error) {
+			alert('ERROR SEARCHING RECIPE');
+		}
 	}
 };
 
@@ -48,6 +55,12 @@ elements.searchForm.addEventListener('submit', (e) => {
 	e.preventDefault();
 	controlSearch();
 });
+
+// testing
+// window.addEventListener('load', (e) => {
+// 	e.preventDefault();
+// 	controlSearch();
+// });
 
 elements.searchResPage.addEventListener('click', (e) => {
 	const btn = e.target.closest('.btn-inline');
@@ -88,19 +101,32 @@ const controlRecipe = async () => {
 		//- Create recipe object (remember add it to 'state' object) and pass the 'id'.
 		state.recipe = new Recipe(id);
 
-		// 4. Get recipe data & parse the ingredients
-		//- Call the 'getRecipe' function. Tips: it's on state object and it's await.
-		await state.recipe.getRecipe();
+		// testing
+		//window.r = state.recipe;
 
-		// 5. Calculte servings and time by calling the method
-		//- call the 'calcTime' and 'calcServing' function. Tips: it's on state object.
-		state.recipe.calcTime();
-		state.recipe.calcServing();
+		try {
+			// 4. Get recipe data & parse the ingredients
+			//- Call the 'getRecipe' function. Tips: it's on state object and it's await.
+			await state.recipe.getRecipe();
+			state.recipe.parseIngredients();
 
-		// 6. Render recipe to UI.
-		//- conslo.log 'state.recipe'.
-		console.log(state.recipe);
+			// 5. Calculte servings and time by calling the method
+			//- call the 'calcTime' and 'calcServing' function. Tips: it's on state object.
+			state.recipe.calcTime();
+			state.recipe.calcServings();
+
+			// 6. Render recipe to UI.
+			//- conslo.log 'state.recipe'.
+			console.log(state.recipe);
+		} catch (error) {
+			alert('ERROR LOADING RECIPE');
+		}
 	}
 };
 
-window.addEventListener('hashchange', controlRecipe);
+// window.addEventListener('hashchange', controlRecipe);
+// window.addEventListener('load', controlRecipe);
+
+['hashchange', 'load'].forEach((cur) => {
+	window.addEventListener(cur, controlRecipe);
+});
