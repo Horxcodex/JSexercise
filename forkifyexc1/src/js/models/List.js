68221661c0066aa3a0b8f85c6b27906cf0b395e1
Horxcodex <fearofnear@gmail.com>
2020-02-10 13:12:18 +0700

    Completed List model

---
+++ forkifyexc1/src/js/models/List.js
@@ -0,0 +1,33 @@
+import uniqid from 'uniqid';
+
+export default class List {
+	constructor() {
+		this.items = [];
+	}
+
+	addItem(count, unit, ingredient) {
+		const item = {
+			id: uniqid(),
+			count: count,
+			unit: unit,
+			ingredient: ingredient
+		};
+		this.items.push(item);
+
+		return item;
+	}
+
+	deleteItem(id) {
+		/* const index = this.items.findIndex((cur) => {
+			cur.id === id;
+		}); */
+
+		const index = this.items.findIndex((cur) => cur.id === id);
+
+		this.items.splice(index, 1);
+	}
+
+	updateCount(id, newCount) {
+		this.items.find((cur) => cur.id === id).count = newCount;
+	}
+}
